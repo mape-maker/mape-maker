d752b98ed4000523e5eb5a3f3aff488c06f911e2
sukhmandk <sdkkaur@ucdavis.edu>
2019-09-10 02:00:37 -0700

    added more tests

--- test/known_failures_s/known_failures.py
+++ test/known_failures_s/known_failures.py
@@ -143,14 +143,14 @@ class TestUM(unittest.TestCase):
         '''
         The operational examples are giving very bad scenarios as of now.
         This test is using rts_wind test
-        target mape             : dataset's mape (=186.39%)
+        target mape             : 90 (dataset's mape =186.39%)
         the input start date    : "2020-01-01 01:00:00"
         the input end date      : "2020-12-29 23:00:00"
         the sim start date      : "2020-12-30 00:00:00"
         the sim end date        : "2020-12-31 23:00:00"
         '''
         print("Running ", str(self.id()).split('.')[2])
-        # python -m mape_maker "mape_maker/samples/rts_gmlc/wind_operations_example.csv" -st "actuals" -n 5 -bp "ARMA" -is "2020-1-1 01:00:00" -ie "2020-12-29 23:00:00" -sd "2020-12-30 00:00:00" -ed "2020-12-31 23:00:00" -o "load_operations_example" -s 1234
+        # python -m mape_maker "mape_maker/samples/rts_gmlc/wind_operations_example.csv" -st "actuals" -n 5 -bp "ARMA" -is "2020-1-1 01:00:00" -ie "2020-12-29 23:00:00" -sd "2020-12-30 00:00:00" -ed "2020-12-31 23:00:00" -o "wind_operations_example_1" -s 1234
         parm_dict                           = self._base_dict()
         parm_dict["input_file"]             = self.wind_ops
         parm_dict["simulated_timeseries"]   = "actuals"
@@ -177,14 +177,14 @@ class TestUM(unittest.TestCase):
         '''
         The operational examples are giving very bad scenarios as of now.
         This test is using rts_wind test
-        target mape             : dataset's mape (=186.39%)
+        target mape             : 90 (dataset's mape =186.39%)
         the input start date    : "2020-01-01 01:00:00"
         the input end date      : "2020-12-29 23:00:00"
         the sim start date      : "2020-12-30 12:00:00" ****
         the sim end date        : "2020-12-31 23:00:00"
         '''
         print("Running ", str(self.id()).split('.')[2])
-        # python -m mape_maker "mape_maker/samples/rts_gmlc/wind_operations_example.csv" -st "actuals" -n 5 -bp "ARMA" -is "2020-1-1 01:00:00" -ie "2020-12-29 23:00:00" -sd "2020-12-30 12:00:00" -ed "2020-12-31 23:00:00" -o "load_operations_example" -s 1234
+        # python -m mape_maker "mape_maker/samples/rts_gmlc/wind_operations_example.csv" -st "actuals" -n 5 -bp "ARMA" -is "2020-1-1 01:00:00" -ie "2020-12-29 23:00:00" -sd "2020-12-30 12:00:00" -ed "2020-12-31 23:00:00" -o "wind_operations_example_2" -s 1234
         parm_dict                           = self._base_dict()
         parm_dict["input_file"]             = self.wind_ops
         parm_dict["simulated_timeseries"]   = "actuals"
@@ -207,6 +207,72 @@ class TestUM(unittest.TestCase):
         plot1 = "mmFinalFig.png"
         shutil.move(plot1, self.saving + dir_sep + parm_dict["output_dir"] )
 
+    def test_WIND_operations_example_3(self):
+        print("OPERATIONAL TESTS")
+        '''
+        The operational examples are giving very bad scenarios as of now.
+        This test is using rts_wind test
+        target mape             : dataset's mape (=186.39%)
+        the input start date    : "2020-01-01 01:00:00"
+        the input end date      : "2020-12-29 23:00:00"
+        the sim start date      : "2020-12-30 00:00:00"
+        the sim end date        : "2020-12-31 23:00:00"
+        '''
+        print("Running ", str(self.id()).split('.')[2])
+        # python -m mape_maker "mape_maker/samples/rts_gmlc/wind_operations_example.csv" -st "actuals" -n 5 -bp "ARMA" -is "2020-1-1 01:00:00" -ie "2020-12-29 23:00:00" -sd "2020-12-30 00:00:00" -ed "2020-12-31 23:00:00" -o "wind_operations_example_3" -s 1234
+        parm_dict                           = self._base_dict()
+        parm_dict["input_file"]             = self.wind_ops
+        parm_dict["simulated_timeseries"]   = "actuals"
+        parm_dict["number_simulations"]     = 5
+        parm_dict["base-process"]           = "ARMA"
+        parm_dict["input_start_dt"]         = datetime(year=2020, month=1, day=1, hour=0, minute=0, second=0)
+        parm_dict["input_end_dt"]           = datetime(year=2020, month=12, day=29, hour=23, minute=0, second=0)
+        parm_dict["simulation_start_dt"]    = datetime(year=2020, month=12, day=30, hour=0, minute=0, second=0)
+        parm_dict["simulation_end_dt"]      = datetime(year=2020, month=12, day=31, hour=23, minute=0, second=0)
+        parm_dict["output_dir"]             = "wind_operations_example_3"
+        parm_list = list(parm_dict.values())
+        # run the test
+        mapemain.main_func(*parm_list)
+
+        # save the output dir to the sub temporary directory
+        output_dir_path = self.cwd  + dir_sep + parm_dict["output_dir"]
+        shutil.move(output_dir_path, self.saving)
+
+        plot1 = "mmFinalFig.png"
+        shutil.move(plot1, self.saving + dir_sep + parm_dict["output_dir"] )
+
+    def test_WIND_operations_example_4(self):
+        '''
+        The operational examples are giving very bad scenarios as of now.
+        This test is using rts_wind test
+        target mape             : dataset's mape (=186.39%)
+        the input start date    : "2020-01-01 01:00:00"
+        the input end date      : "2020-12-29 23:00:00"
+        the sim start date      : "2020-12-30 12:00:00" ****
+        the sim end date        : "2020-12-31 23:00:00"
+        '''
+        print("Running ", str(self.id()).split('.')[2])
+        # python -m mape_maker "mape_maker/samples/rts_gmlc/wind_operations_example.csv" -st "actuals" -n 5 -bp "ARMA" -is "2020-1-1 01:00:00" -ie "2020-12-29 23:00:00" -sd "2020-12-30 12:00:00" -ed "2020-12-31 23:00:00" -o "wind_operations_example_4" -s 1234
+        parm_dict                           = self._base_dict()
+        parm_dict["input_file"]             = self.wind_ops
+        parm_dict["simulated_timeseries"]   = "actuals"
+        parm_dict["number_simulations"]     = 5
+        parm_dict["base-process"]           = "ARMA"
+        parm_dict["input_start_dt"]         = datetime(year=2020, month=1, day=1, hour=0, minute=0, second=0)
+        parm_dict["input_end_dt"]           = datetime(year=2020, month=12, day=29, hour=23, minute=0, second=0)
+        parm_dict["simulation_start_dt"]    = datetime(year=2020, month=12, day=12, hour=0, minute=0, second=0)
+        parm_dict["simulation_end_dt"]      = datetime(year=2020, month=12, day=31, hour=23, minute=0, second=0)
+        parm_dict["output_dir"]             = "wind_operations_example_4"
+        parm_list = list(parm_dict.values())
+        # run the test
+        mapemain.main_func(*parm_list)
+
+        # save the output dir to the sub temporary directory
+        output_dir_path = self.cwd  + dir_sep + parm_dict["output_dir"]
+        shutil.move(output_dir_path, self.saving)
+
+        plot1 = "mmFinalFig.png"
+        shutil.move(plot1, self.saving + dir_sep + parm_dict["output_dir"] )
 
 if __name__ == "__main__":
     unittest.main()
