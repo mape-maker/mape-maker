c4248e46fd8b8afbc64d4e15615361277cb3c754
wyy0607 <yywwu@ucdavis.edu>
2020-08-04 15:59:29 -0700

    modify test

--- test/q_fast_CAISO_wind_tests.py
+++ test/q_fast_CAISO_wind_tests.py
@@ -10,7 +10,6 @@ quick_test = False
 skip_all_but_one = False
 
 
-
 class TestUM(unittest.TestCase):
 
     @classmethod
@@ -24,21 +23,21 @@ class TestUM(unittest.TestCase):
                            dir_sep + "wind_total_forecast_actual_070113_063015.csv"
         self.parser = mapemain.make_parser()
 
-    # def test_CAISO_wind_actuals_ARMA_seed_1234(self):
-    #     parm_dict = {'-xf': self.wind_data, '-s': "1234",
-    #                  '-is': str(datetime(year=2014, month=7, day=1, hour=0, minute=0, second=0)),
-    #                  '-ie': str(datetime(year=2014, month=8, day=1, hour=0, minute=0, second=0)),
-    #                  '-ss': str(datetime(year=2014, month=7, day=2, hour=0, minute=0, second=0)),
-    #                  '-se': str(datetime(year=2014, month=7, day=31, hour=0, minute=0, second=0))}
-    #     parm_list = []
-    #     for i, j in parm_dict.items():
-    #         if j is not None:
-    #             parm_list += [i, j]
-    #         else:
-    #             parm_list += [i]
-    #     # run the test
-    #     args = self.parser.parse_args(parm_list)
-    #     mapemain.main(args)
+    def test_CAISO_wind_actuals_ARMA_seed_1234(self):
+        parm_dict = {'-xf': self.wind_data, '-s': "1234",
+                     '-is': str(datetime(year=2014, month=7, day=1, hour=0, minute=0, second=0)),
+                     '-ie': str(datetime(year=2014, month=8, day=1, hour=0, minute=0, second=0)),
+                     '-ss': str(datetime(year=2014, month=7, day=2, hour=0, minute=0, second=0)),
+                     '-se': str(datetime(year=2014, month=7, day=31, hour=0, minute=0, second=0))}
+        parm_list = []
+        for i, j in parm_dict.items():
+            if j is not None:
+                parm_list += [i, j]
+            else:
+                parm_list += [i]
+        # run the test
+        args = self.parser.parse_args(parm_list)
+        mapemain.main(args)
     #
     def test_CAISO_wind_forecasts_iid_seed_1134(self):
         parm_dict = {"-xf": self.wind_data, "-s": "1134", "-bp": "iid", "-f": "forecasts",
@@ -56,38 +55,37 @@ class TestUM(unittest.TestCase):
         args = self.parser.parse_args(parm_list)
         mapemain.main(args)
 
+    def test_CAISO_wind_actuals_iid_seed_1134(self):
+        parm_dict = {"-xf": self.wind_data, "-bp": "iid", "-s": "1134",
+                     "-is": str(datetime(year=2014, month=1, day=1, hour=0, minute=0, second=0)),
+                     "-ie": str(datetime(year=2014, month=10, day=1, hour=0, minute=0, second=0)),
+                     "-ss": str(datetime(year=2014, month=1, day=2, hour=0, minute=0, second=0)),
+                     "-se": str(datetime(year=2014, month=9, day=30, hour=0, minute=0, second=0))}
+        parm_list = []
+        for i, j in parm_dict.items():
+            if j is not None:
+                parm_list += [i, j]
+            else:
+                parm_list += [i]
+        # run the test
+        args = self.parser.parse_args(parm_list)
+        mapemain.main(args)
 
-    # def test_CAISO_wind_actuals_iid_seed_1134(self):
-    #     parm_dict = {"-xf": self.wind_data, "-bp": "iid", "-s": "1134",
-    #                  "-is": str(datetime(year=2014, month=1, day=1, hour=0, minute=0, second=0)),
-    #                  "-ie": str(datetime(year=2014, month=10, day=1, hour=0, minute=0, second=0)),
-    #                  "-ss": str(datetime(year=2014, month=1, day=2, hour=0, minute=0, second=0)),
-    #                  "-se": str(datetime(year=2014, month=9, day=30, hour=0, minute=0, second=0))}
-    #     parm_list = []
-    #     for i, j in parm_dict.items():
-    #         if j is not None:
-    #             parm_list += [i, j]
-    #         else:
-    #             parm_list += [i]
-    #     # run the test
-    #     args = self.parser.parse_args(parm_list)
-    #     mapemain.main(args)
-    #
-    # def test_CAISO_wind_forecasts_ARMA_seed_1234(self):
-    #     parm_dict = {"-xf": self.wind_data, "-f": "forecasts", "-s": "1134",
-    #                  "-is": str(datetime(year=2014, month=7, day=1, hour=0, minute=0, second=0)),
-    #                  "-ie": str(datetime(year=2014, month=8, day=1, hour=0, minute=0, second=0)),
-    #                  "-ss": str(datetime(year=2014, month=7, day=2, hour=0, minute=0, second=0)),
-    #                  "-se": str(datetime(year=2014, month=7, day=31, hour=0, minute=0, second=0))}
-    #     parm_list = []
-    #     for i, j in parm_dict.items():
-    #         if j is not None:
-    #             parm_list += [i, j]
-    #         else:
-    #             parm_list += [i]
-    #     # run the test
-    #     args = self.parser.parse_args(parm_list)
-    #     mapemain.main(args)
+    def test_CAISO_wind_forecasts_ARMA_seed_1234(self):
+        parm_dict = {"-xf": self.wind_data, "-f": "forecasts", "-s": "1134",
+                     "-is": str(datetime(year=2014, month=7, day=1, hour=0, minute=0, second=0)),
+                     "-ie": str(datetime(year=2014, month=8, day=1, hour=0, minute=0, second=0)),
+                     "-ss": str(datetime(year=2014, month=7, day=2, hour=0, minute=0, second=0)),
+                     "-se": str(datetime(year=2014, month=7, day=31, hour=0, minute=0, second=0))}
+        parm_list = []
+        for i, j in parm_dict.items():
+            if j is not None:
+                parm_list += [i, j]
+            else:
+                parm_list += [i]
+        # run the test
+        args = self.parser.parse_args(parm_list)
+        mapemain.main(args)
 
 
 if __name__ == "__main__":
