d6b2381ca01789cd4a6508d9d26af3a216b6058d
wyy0607 <yywwu@ucdavis.edu>
2020-08-13 18:11:25 -0700

    modify test

--- test/operation_test.py
+++ test/operation_test.py
@@ -18,61 +18,61 @@ class TestUM(unittest.TestCase):
     def setUpClass(self):
         self.parser = mapemain.make_parser()
 
-    # def test_CAISO_actuals_ARMA_seed_1234(self):
-    #     print("Running ", str(self.id()).split('.')[2])
-    #     parm_dict = {"-xf": file_path + dir_sep + "CAISO_wind_operational_data.csv",
-    #                  "-s": "1234",
-    #                  "-is": str(datetime(year=2013, month=7, day=1, hour=0, minute=0, second=0)),
-    #                  "-ie": str(datetime(year=2013, month=12, day=30, hour=23, minute=0, second=0)),
-    #                  "-ss": str(datetime(year=2015, month=6, day=29, hour=23, minute=0, second=0)),
-    #                  "-se": str(datetime(year=2015, month=6, day=30, hour=23, minute=0, second=0))}
-    #     parm_list = []
-    #     for i, j in parm_dict.items():
-    #         if j is not None:
-    #             parm_list += [i, j]
-    #         else:
-    #             parm_list += [i]
-    #     # run the test
-    #     args = self.parser.parse_args(parm_list)
-    #     mapemain.main(args)
-    #
-    # def test_CAISO_actuals_iid_seed_1234(self):
-    #     print("Running ", str(self.id()).split('.')[2])
-    #     parm_dict = {"-xf": file_path + dir_sep + "CAISO_wind_operational_data.csv",
-    #                  "-bp": "iid", "-s": "1234",
-    #                  "-is": str(datetime(year=2013, month=7, day=1, hour=0, minute=0, second=0)),
-    #                  "-ie": str(datetime(year=2013, month=12, day=30, hour=23, minute=0, second=0)),
-    #                  "-ss": str(datetime(year=2015, month=6, day=29, hour=23, minute=0, second=0)),
-    #                  "-se": str(datetime(year=2015, month=6, day=30, hour=23, minute=0, second=0))}
-    #     parm_list = []
-    #     for i, j in parm_dict.items():
-    #         if j is not None:
-    #             parm_list += [i, j]
-    #         else:
-    #             parm_list += [i]
-    #     # run the test
-    #     args = self.parser.parse_args(parm_list)
-    #     mapemain.main(args)
-    #
-    # def test_wind_actuals_ARMA_seed_1234(self):
-    #     print("Running ", str(self.id()).split('.')[2])
-    #     parm_dict = {"-xf": file_path + dir_sep + "rts_gmlc" + dir_sep + "WIND_forecasts_actuals.csv",
-    #                  "-s": "1234",
-    #                  "-is": str(datetime(year=2020, month=2, day=1, hour=0, minute=0, second=0)),
-    #                  "-ie": str(datetime(year=2020, month=10, day=31, hour=23, minute=0, second=0)),
-    #                  "-ss": str(datetime(year=2020, month=11, day=1, hour=0, minute=0, second=0)),
-    #                  "-se": str(datetime(year=2020, month=11, day=7, hour=0, minute=0, second=0))}
-    #     parm_list = []
-    #     for i, j in parm_dict.items():
-    #         if j is not None:
-    #             parm_list += [i, j]
-    #         else:
-    #             parm_list += [i]
-    #     # run the test
-    #     args = self.parser.parse_args(parm_list)
-    #     with self.assertRaises(RuntimeError) as context:
-    #         mapemain.main(args)
-    #     self.assertTrue('< 1, there is a prevalence of high power input in the SID' in str(context.exception))
+    def test_CAISO_actuals_ARMA_seed_1234(self):
+        print("Running ", str(self.id()).split('.')[2])
+        parm_dict = {"-xf": file_path + dir_sep + "CAISO_wind_operational_data.csv",
+                     "-s": "1234",
+                     "-is": str(datetime(year=2013, month=7, day=1, hour=0, minute=0, second=0)),
+                     "-ie": str(datetime(year=2013, month=12, day=30, hour=23, minute=0, second=0)),
+                     "-ss": str(datetime(year=2015, month=6, day=29, hour=23, minute=0, second=0)),
+                     "-se": str(datetime(year=2015, month=6, day=30, hour=23, minute=0, second=0))}
+        parm_list = []
+        for i, j in parm_dict.items():
+            if j is not None:
+                parm_list += [i, j]
+            else:
+                parm_list += [i]
+        # run the test
+        args = self.parser.parse_args(parm_list)
+        mapemain.main(args)
+
+    def test_CAISO_actuals_iid_seed_1234(self):
+        print("Running ", str(self.id()).split('.')[2])
+        parm_dict = {"-xf": file_path + dir_sep + "CAISO_wind_operational_data.csv",
+                     "-bp": "iid", "-s": "1234",
+                     "-is": str(datetime(year=2013, month=7, day=1, hour=0, minute=0, second=0)),
+                     "-ie": str(datetime(year=2013, month=12, day=30, hour=23, minute=0, second=0)),
+                     "-ss": str(datetime(year=2015, month=6, day=29, hour=23, minute=0, second=0)),
+                     "-se": str(datetime(year=2015, month=6, day=30, hour=23, minute=0, second=0))}
+        parm_list = []
+        for i, j in parm_dict.items():
+            if j is not None:
+                parm_list += [i, j]
+            else:
+                parm_list += [i]
+        # run the test
+        args = self.parser.parse_args(parm_list)
+        mapemain.main(args)
+
+    def test_wind_actuals_ARMA_seed_1234(self):
+        print("Running ", str(self.id()).split('.')[2])
+        parm_dict = {"-xf": file_path + dir_sep + "rts_gmlc" + dir_sep + "WIND_forecasts_actuals.csv",
+                     "-s": "1234",
+                     "-is": str(datetime(year=2020, month=2, day=1, hour=0, minute=0, second=0)),
+                     "-ie": str(datetime(year=2020, month=10, day=31, hour=23, minute=0, second=0)),
+                     "-ss": str(datetime(year=2020, month=11, day=1, hour=0, minute=0, second=0)),
+                     "-se": str(datetime(year=2020, month=11, day=7, hour=0, minute=0, second=0))}
+        parm_list = []
+        for i, j in parm_dict.items():
+            if j is not None:
+                parm_list += [i, j]
+            else:
+                parm_list += [i]
+        # run the test
+        args = self.parser.parse_args(parm_list)
+        with self.assertRaises(RuntimeError) as context:
+            mapemain.main(args)
+        self.assertTrue('< 1, there is a prevalence of high power input in the SID' in str(context.exception))
 
     def test_wind_forecasts_ARMA_seed_1234(self):
         print("Running ", str(self.id()).split('.')[2])
